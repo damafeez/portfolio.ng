180c0ff9805c2884c4e9e081656ca9794e9883eb
Afeez Awoyemi <damafeez@gmail.com>
2020-06-13 12:32:17 +0100

    chore(templates): unset redundant meta name

--- pages/templates.vue
+++ pages/templates.vue
@@ -9,7 +9,6 @@ export default {
   middleware({ app, store, route }) {
     const name = route.name.split('-')[1]
     if (name) {
-      store.dispatch('document/editSchema', ['_meta.name', name])
       store.dispatch(
         'document/setSchema',
         getSchema(name, store.state.document.schema),
