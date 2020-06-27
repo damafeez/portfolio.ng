661f77c7e643ba01f50235fea005854367b4fac4
Afeez Awoyemi <damafeez@gmail.com>
2020-06-27 22:01:56 +0100

    feat(hero): add hero-jade

---
+++ components/sections/HeroJade.vue
@@ -0,0 +1,25 @@
+<template>
+  <section
+    is="p-img"
+    address="profile.images[0]"
+    :overlay="0.4"
+    class="hero bg-secondary text-on-secondary bg-cover bg-no-repeat pb-32 text-center flex flex-col items-center"
+  >
+    <h1 class="mt-56 mb-6 max-w-2xl">
+      Hi, <br />
+      I'm
+      <span is="p-text" address="profile.name"></span>.
+    </h1>
+    <p is="p-text" address="profile.description" class="max-w-xl"></p>
+    <button
+      is="p-text"
+      address="cta[0].text"
+      class="bg-primary my-10 text-on-primary"
+    ></button>
+  </section>
+</template>
+<script>
+export default {
+  tags: ['somolu', 'dev', 'hero'],
+}
+</script>
