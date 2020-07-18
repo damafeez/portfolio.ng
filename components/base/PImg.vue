30a7a157d1d9dc862d2134e62e751ecbfc68e46b
Afeez Awoyemi <damafeez@gmail.com>
2020-07-18 14:24:38 +0100

    feat(img): image overlay

--- components/base/PImg.vue
+++ components/base/PImg.vue
@@ -1,9 +1,7 @@
 <script>
 import { get, inRange } from 'lodash'
-import { mapGetters } from 'vuex'
 import { changeFeed, changeFeedProps } from '~/mixins'
 import { TEMPLATE_EDITOR_UPLOAD_IMAGE } from '~/constants'
-import { wait } from '~/utils'
 
 export default {
   // mount file uploader from this component
@@ -16,48 +14,25 @@ export default {
     },
     overlay: {
       type: [Number, String],
-      default: 0,
-      validator: val => inRange(val, 0, 1.1),
+      default: null,
+      validator: val => inRange(val, 0, 1.0000001),
     },
   },
-  data() {
-    return {
-      shadowOverlay: null,
-    }
-  },
   computed: {
-    ...mapGetters({ currentThemeName: 'document/currentThemeName' }),
     imgUrl() {
       return get(this.schema, this.address, '')
     },
     tagName() {
       return this.$vnode.data.tag || 'img'
     },
+    brightness() {
+      return this.overlay && `brightness(${this.overlay * 100}%)`
+    },
   },
   watch: {
     imgUrl(url) {
       if (this.$el.tagName === 'IMG') this.$el.setAttribute('src', url)
       else this.$el.style.backgroundImage = `url(${this.imgUrl})`
-    },
-    currentThemeName: {
-      async handler() {
-        if (process.client) {
-          if (!this.overlay) {
-            this.shadowOverlay = undefined
-            return
-          }
-          await wait(500)
-          const { backgroundColor } = getComputedStyle(this.$el)
-          const overlayColor = backgroundColor.replace(
-            /^rgba?\((\d+,\s*\d+,\s*\d+)(,\s*\d+)?\)$/,
-            `rgba($1 ,${this.overlay})`,
-          )
-          // TODO: handle initial shadow
-          const shadowOverlay = `inset 0 0 0 100vw ${overlayColor}`
-          this.shadowOverlay = shadowOverlay
-        }
-      },
-      immediate: true,
     },
   },
   methods: {
@@ -74,29 +49,55 @@ export default {
       this.$el.classList.remove('stop-cursor-propagation')
     },
   },
-  render(createElement) {
+  render(h) {
+    const { tagName, imgUrl, brightness } = this
     const slot =
       typeof this.$scopedSlots.default === 'function'
         ? this.$scopedSlots.default({
-            value: this.href,
+            value: imgUrl,
           })
         : null
+    const attrs = {}
+    const style = {}
+    if (tagName === 'img') {
+      attrs.src = imgUrl
+      brightness && (style.filter = brightness)
+    } else {
+      style.backgroundImage = `url(${imgUrl})`
+      brightness && (style.position = 'relative')
+    }
+
     return this.tagName
-      ? createElement(
+      ? h(
           this.tagName,
           {
             on: this.$listeners,
-            attrs: {
-              src: this.tagName === 'img' ? this.imgUrl : undefined,
-            },
-            style: {
-              backgroundImage:
-                this.tagName !== 'img' ? `url(${this.imgUrl})` : undefined,
-              boxShadow: this.shadowOverlay,
-              transition: 'box-shadow 0.2s cubic-bezier(0.075, 0.82, 0.165, 1)',
-            },
+            attrs,
+            style,
           },
-          slot,
+          tagName === 'img' || !brightness
+            ? slot
+            : [
+                h('div', {
+                  style: {
+                    backdropFilter: `brightness(${this.overlay * 100}%)`,
+                    position: 'absolute',
+                    width: '100%',
+                    height: '100%',
+                    top: 0,
+                    left: 0,
+                  },
+                }),
+                h(
+                  'div',
+                  {
+                    style: {
+                      position: 'relative',
+                    },
+                  },
+                  slot,
+                ),
+              ],
         )
       : slot
   },
--- components/sections/HeroSomolu.vue
+++ components/sections/HeroSomolu.vue
@@ -1,10 +1,10 @@
 <template>
   <section
     is="p-img"
+    overlay="0.7"
     address="profile.images[0]"
-    class="hero bg-secondary text-on-secondary bg-right-top bg-no-repeat pb-32"
+    class="hero bg-secondary text-on-secondary bg-right-top bg-no-repeat pb-32 pt-5"
   >
-    <nav class="neg-margin pad-x py-6"></nav>
     <h1 class="mt-48 mb-6 max-w-2xl">
       Hi, <br />
       I'm
